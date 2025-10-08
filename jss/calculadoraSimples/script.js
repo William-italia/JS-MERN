b7ba9fe49a7eb60bbaa529ed165209df7d13d65b
william-italia <williamitalia70@outlook.com>
2025-10-08 17:49:23 -0300

    up

--- jss/calculadoraSimples/script.js
+++ jss/calculadoraSimples/script.js
@@ -2,13 +2,24 @@ function criaCalculadora() {
 
     return {
         display: document.getElementById('display-p'),
+        recent: document.getElementById('recent'),
+        historico: [],
 
         inicia() {
+            
+            const historicoSalvo = localStorage.getItem('historico');
+            
+            if(historicoSalvo) {
+                this.historico = JSON.parse(historicoSalvo);
+                this.imprimeHistorico(this.historico);
+            }
+            
             this.cliqueBotoes();
         },
 
         realizaConta() {
             let conta = this.display.textContent;
+            let resultado = '';
 
             try {
 
@@ -16,15 +27,19 @@ function criaCalculadora() {
                     return alert('paia mano');   
                 }
                 
-                conta = eval(conta);
-                console.log(conta);
+                resultado = eval(conta);
                 
-                if(!conta) {
+                if(!resultado) {
                     alert('Conta Invalida');
                     return;
                 }
 
-                this.display.textContent = String(conta);
+                let formatado = Number(resultado.toFixed(2));
+
+                this.addHistorico(this.formataOperador(conta), formatado);
+                this.display.textContent = String(formatado);
+                console.log(this.historico);
+                
 
             } catch(e) {
                 alert('Conta Invalida!');
@@ -53,11 +68,53 @@ function criaCalculadora() {
                     this.realizaConta();
                 }
 
+                if(el.classList.contains('recent-btn')) {
+                    this.abreFechaHistorico();
+                    this.imprimeHistorico(this.historico);
+                }
+
             }.bind(this));
         },
 
         btnParaDipslay(valor) {
             this.display.textContent += valor;
+        },
+
+        abreFechaHistorico() {
+             this.recent.classList.toggle('active');
+        },
+
+        formataOperador(str) {
+            
+            // const operadores = ['+', '-', '*', '/'];
+            // const operador = operadores.find(op => str.includes(op));
+
+            // if(operador) {
+            //     return str.replace(operador, ` ${operador} `);
+            // };
+
+            return str.replace(/([+\-*/])/g, " $1 ");
+        },
+
+        imprimeHistorico(lista) {
+
+            this.recent.innerHTML = '';
+            
+            for (const conta of lista) {
+                const li = document.createElement('li');
+                li.textContent = conta;
+                this.recent.appendChild(li);
+            }
+
+        },
+
+        addHistorico(str, resultado) {
+
+            let conta = `${str} = ${resultado}`;
+
+            this.historico.unshift(conta);
+
+            localStorage.setItem('historico', JSON.stringify(this.historico));
         },
 
         clearDisplay() {
