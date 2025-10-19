befe3c0d0a381d1ec65b10755ed4412a977a751a
william-italia <williamitalia70@outlook.com>
2025-10-18 22:02:35 -0300

    up

---
+++ node/6expressNodemon/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {res.send('inicio')});
+
+app.post('/', (req, res) => {res.send('recebi form')});
+
+app.get('/contato', (req, res) => {res.send('pagina contato')});
+
+app.listen(3000, () => {
+    console.log('Start Server');
+    console.log('Acessar: http://localhost:3000');
+});
